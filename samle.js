cbd293d5da8a2e5853e3c5d289b2797e19ee12d4
rei0101b <rei0101b@gmail.com>
2020-05-06 00:27:03 +0900

    スカラー倍

--- samle.js
+++ samle.js
@@ -1,17 +1,10 @@
 let V = [5, -3];
 
-let W = [-2, 6];
+let scalar = 2.0
 
-let A = [
-  V[0] + W[0],
-  V[1] + W[1]
+let W = [
+  V[0] * scalar,
+  V[1] * scalar
 ]
 
-console.log(A)
-
-let S = [
-  V[0] - W[0],
-  V[0] - W[1]
-]
-
-console.log(S)
+console.log(W)
