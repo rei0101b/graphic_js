b39c6828f79e05331b83b19af6b0a175f6a7e7dc
rei0101b <rei0101b@gmail.com>
2020-05-05 23:25:21 +0900

    単位ベクトル

--- samle.js
+++ samle.js
@@ -1,9 +1,7 @@
-let A = [-2, 2]
-let B = [3, -1]
+let V = [5, -3]
 
-let V = [
-  B[0] - A[0],
-  B[1] - A[1],
-]
+let L = Math.sqrt(V[0] * V[0] + V[1] * V[1]) 
+V[0] /= L
+V[1] /= L
 
 console.log(V)
