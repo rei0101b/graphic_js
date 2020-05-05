a166272015f7619aca0ada70ffad4341a6ced6c3
rei0101b <rei0101b@gmail.com>
2020-05-05 22:30:57 +0900

    定量移動

--- samle.js
+++ samle.js
@@ -1,6 +1,18 @@
-let rad = 0 * Math.PI / 180;
 
-let s = Math.sin(rad);
-let c = Math.cos(rad);
-console.log(s)
-console.log(c)
+let r = 2
+let theta = 120
+let A = [0.0, 0.0];
+let radian = degToRad(theta);
+
+let s = Math.sin(radian);
+let c = Math.cos(radian);
+
+let B = [
+  A[0] + c * r,
+  A[1] + s * r,
+]
+
+function degTorad(theta) {
+  return theta * Math.PI / 180
+}
+
