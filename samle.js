85f2e377aee3f7a1fd91713cb7cdce689a41751c
rei0101b <rei0101b@gmail.com>
2020-05-06 02:31:20 +0900

    ベクトル内積　cos

--- samle.js
+++ samle.js
@@ -1,8 +1,23 @@
-let v0 = [1,5]
-let v1 = [-2, 8]
+let V = [5,1]
+let W = [-2, 3]
 
-console.log(dot(v0, v1))
+V = normalize(V)
+W = normalize(W)
+
+let dotValue = dot(V,W)
+
+let rad = Math.acos(dotValue)
+
+let deg = rad / Math.PI * 180
+
+console.log(rad)
+console.log(deg)
+
+function normalize(v) {
+  let len = Math.sqrt(v[0] * v[0] + v[1] * v[1])
+  return [v[0]/len, v[1]/len]
+}
 
 function dot(v0, v1) {
-  return (v0[0] * v1[0]) + (v0[1] * v1[1])
+  return (v0[0] * v1[0] + v0[1] * v1[1])
 }
