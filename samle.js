4baaa067125f66b493e5bef85440fdb3d61d1bb5
rei0101b <rei0101b@gmail.com>
2020-05-07 17:05:51 +0900

    補完関数

--- samle.js
+++ samle.js
@@ -1,40 +1,15 @@
-let V = [5,1]
-let W = [-2, 3]
-
-V = normalize(V)
-W = normalize(W)
-
-let dotValue = dot(V,W)
-
-let rad = Math.acos(dotValue)
-
-let deg = rad / Math.PI * 180
-
-console.log(rad)
-console.log(deg)
-
-function normalize(v) {
-  let len = Math.sqrt(v[0] * v[0] + v[1] * v[1])
-  return [v[0]/len, v[1]/len]
+function linear(t) {
+  return t
 }
 
-function dot(v0, v1) {
-  return (v0[0] * v1[0] + v0[1] * v1[1])
+function easeInQuad(t) {
+  return t*t
 }
 
-function crooss(v0, v1) {
-  return [
-    v0[1] * v1[2] - v0[2] * v1[1],
-    v0[2] * v1[0] - v0[0] * v1[2],
-    v0[0] * v1[1] - v0[1] * v1[0],
-  ]
+function easeOutQuad(t) {
+  return t * (2-t)
 }
 
-function rotate2D(vec, radian) {
-  let sin = Math.sin(radian)
-  let cos = Math.cos(radian)
-  return [
-    vec[0] * cos + vec[1] * -sin,
-    vec[0] * sin + vec[1] * cos
-  ]
+function easeInOutQuad(t) {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
 }
